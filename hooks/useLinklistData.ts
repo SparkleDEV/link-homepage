7a812e3cb822b47aba59f9bb076461a6668e3571
Vivian Hemb <vivian.hemb@proton.me>
2023-11-06 21:25:53 +0100

    Implemented data fetching

--- hooks/useLinklistData.ts
+++ hooks/useLinklistData.ts
@@ -1,34 +1,24 @@
 import LinklistData from '@/types/LinklistData'
 
-const useLinklistData = (url: string): LinklistData[] => {
-	// TODO (29 Oct 23): Fetch data from url
-	return [
-		{
-			display: 'Mastodon',
-			url: 'https://tech.lgbt/@Sparkle',
-			icon_url: '/icons/logos/Mastodon.svg'
-		},
-		{
-			display: 'Bluesky',
-			url: 'https://bsky.app/profile/thesparkle.bsky.social',
-			icon_url: '/icons/logos/Bluesky.png'
-		},
-		{
-			display: 'Twitter',
-			url: 'https://twitter.com/YouGotSparkled',
-			icon_url: '/icons/logos/Twitter.svg'
-		},
-		{
-			display: 'GitHub',
-			url: 'https://github.com/SparkleDEV',
-			icon_url: '/icons/logos/GitHub.svg'
-		},
-		{
-			display: 'Pronouns Page',
-			url: 'https://pronouns.page/@Sparkle',
-			icon_url: '/icons/logos/PronounsPage.svg'
+type LinklistDataResult = { data?: LinklistData[]; error?: string }
+
+const useLinklistData = async (): Promise<LinklistDataResult> => {
+	const result: LinklistDataResult = {}
+
+	const res = await fetch(`${process.env.DATA_HOST}/data/linklist.json`, {
+		next: {
+			revalidate: 60
 		}
-	]
+	})
+
+	if (!res.ok) {
+		result.error = 'There was an error fetching the data :('
+		console.error('Could not fetch json data', res)
+	} else {
+		result.data = (await res.json()) as LinklistData[]
+	}
+
+	return result
 }
 
 export default useLinklistData
